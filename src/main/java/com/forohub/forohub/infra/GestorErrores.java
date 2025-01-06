81f126f0c2ab7282487b95d2778c3ee8c7fb019a
Ivan Hernandez <irhernandezl@gmail.com>
2025-01-06 11:14:56 -0500

    API implementando DELETE & PUT para topico

---
+++ src/main/java/com/forohub/forohub/infra/GestorErrores.java
@@ -0,0 +1,28 @@
+package com.forohub.forohub.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GestorErrores {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity gestorError404(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity gestorError400(MethodArgumentNotValidException  e){
+        var errores = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    private record DatosErrorValidacion(String campo, String error) {
+        public DatosErrorValidacion(FieldError error) {
+            this(error.getField(),error.getDefaultMessage());
+        }
+    }
+}
--- src/main/java/com/forohub/forohub/model/topico/Topico.java
+++ src/main/java/com/forohub/forohub/model/topico/Topico.java
@@ -1,11 +1,16 @@
 package com.forohub.forohub.model.topico;
 
 import jakarta.persistence.*;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Delegate;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @Table(name = "topico")
@@ -29,11 +34,23 @@ public class Topico {
     private Long usuarioId;
     private Long cursoId;
 
-    public Topico(DatosRegistroTopico datosRegistroTopico){
+    public Topico(DatosRegistroTopico datosRegistroTopico) {
         this.titulo = datosRegistroTopico.titulo();
         this.mensaje = datosRegistroTopico.mensaje();
         this.status = "ACTIVO";
         this.usuarioId = datosRegistroTopico.usuarioId();
         this.cursoId = datosRegistroTopico.cursoId();
+    }
+
+    public void actualizarDatos(DatosActualizarTopico datosActualizarTopico) {
+        if (datosActualizarTopico.titulo() != null) {
+            this.titulo = datosActualizarTopico.titulo();
+        }
+        if (datosActualizarTopico.mensaje() != null) {
+            this.mensaje = datosActualizarTopico.mensaje();
+        }
+        if (datosActualizarTopico.status() != null) {
+            this.status = datosActualizarTopico.status();
+        }
     }
 }
